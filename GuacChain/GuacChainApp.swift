2713c4b603714f8567e3d9a1c103cee9dcb50ab6
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-11 15:08:01 +0200

    Initial Commit

---
+++ GuacChain/GuacChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuacChainApp.swift
+//  GuacChain
+//
+//  Created by Eric on 11/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuacChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
